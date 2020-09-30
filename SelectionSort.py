387c6da5220310f5becabdf609a410e0fdae4b34
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-09-30 09:33:49 +0530

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,15 @@
+def selection_sort(a):
+    n = len(a)
+    for j in range(n):
+        smallest = j
+        for i in range(j+1,n):
+            if a[i] < a[smallest]:
+                smallest = i
+        a[j], a[smallest] = a[smallest], a[j]
+    print(a)
+
+a = []
+for i in range(6):
+    a.append(input())
+
+selection_sort(a)
