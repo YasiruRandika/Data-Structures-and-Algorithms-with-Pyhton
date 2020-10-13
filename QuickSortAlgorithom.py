72e6d42dc1d537d4ec868fea3514c1aa81f24abd
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-10-13 18:31:15 +0530

    Change Variable Names of the Quick Sort Algorithm

--- QuickSortAlgorithom.py
+++ QuickSortAlgorithom.py
@@ -2,27 +2,30 @@ array = []
 
 for i in range(7):
     array.append(int(input('Enter a number : ')))
-    print(array)
 
-def partition(array, p, r):
-    i = p - 1 #index of smaller element
-    x = array[r] #pivot
+
+# Partition
+def partition(a, p, r):
+    i = p - 1  # index of smaller element
+    x = a[r]  # pivot
 
     for j in range(p, r):
-        if array[j] <= x :
+        if a[j] <= x:
             i = i + 1
-            array[i], array[j] = array[j], array[i]
+            a[i], a[j] = a[j], a[i]
 
-    array[i + 1], array[r] = array[r], array[i+1]
-    return (i + 1)
+    a[i + 1], a[r] = a[r], a[i + 1]
+    return i + 1
 
-def quickSort(array, p, r):
-    if p < r :
-        q = partition(array, p, r)
-        quickSort(array, p, q-1)
-        quickSort(array, q+1, r)
 
-quickSort(array, 0, len(array)-1)
+def quickSort(a, p, r):
+    if p < r:
+        q = partition(a, p, r)
+        quickSort(a, p, q - 1)
+        quickSort(a, q + 1, r)
+
+
+quickSort(array, 0, len(array) - 1)
 
 print('Sorted array list : ')
 
